91471ced17bea3aa58e5d786e32ffba88e3f3c6b
oreoryza <moryzasativa@gmail.com>
2024-12-16 17:44:31 +0700

    add banner branch

--- src/pages/HomePage..jsx
+++ src/pages/HomePage..jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Banner from "../components/Banner";
 
 const HomePage = () => {
   return (
-    <div></div>
+    <div className="mx-32 my-4">
+        <Banner/>
+    </div>
   );
 };
 
