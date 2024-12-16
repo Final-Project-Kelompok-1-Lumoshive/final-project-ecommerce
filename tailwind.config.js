cdfdbf68e9b05eb143b19abce03d2d1846f5c782
oreoryza <moryzasativa@gmail.com>
2024-12-16 21:21:17 +0700

    fix(tailwind): colors extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,12 @@ export default {
       'poppins': ['Poppins', 'sans-serif'],
       'inter': ['Inter', 'sans-serif'],
     },
-    colors: {
-      'black': '#000000',
-      'red': '#DB4444',
-      'green': '#00FF66',
-      'white': '#FFFFFF',
+    extend: {
+      colors: {
+        'red': '#DB4444',
+        'green': '#00FF66',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
