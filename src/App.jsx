f0d59a7cfe5e20540791f9b4480fc6263887dbac
oreoryza <moryzasativa@gmail.com>
2024-12-17 11:26:07 +0700

    fix(git, footer): control issue about footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
