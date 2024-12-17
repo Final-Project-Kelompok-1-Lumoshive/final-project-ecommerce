136cdd3408e1d828f8a78d211b50a708527f61f6
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-12-17 14:39:44 +0700

    feat(ProductCard): update component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,114 +1,84 @@
 import React, { useEffect, useState } from "react";
-import apiClient from "../services/api";
+import Product1 from "../assets/img/product_1.jpg";
 
 const ProductCard = () => {
-  const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [error, setError] = useState(null);
-
-  const fetchProducts = (page) => {
-    apiClient
-      .get(`/products?page=${page}&limit=8`)
-      .then((response) => {
-        setProducts(response.data.products);
-        setTotalPages(response.data.totalPages);
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-        setError("Failed to fetch products");
-      });
-  };
-
-  useEffect(() => {
-    fetchProducts(currentPage);
-  }, [currentPage]);
-
-  if (!products) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prev) => prev - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage((prev) => prev + 1);
-    }
-  };
-
   return (
     <section className="bg-white py-12 px-6">
-      <div className="max-w-6xl mx-auto text-center">
-        {/* Title and Description */}
-        <h2 className="text-3xl font-bold mb-4">All Products</h2>
-        <p className="text-lg mb-8">
-          The products we provide only for you as our service are selected from
-          the best products with number 1 quality in the world
-        </p>
+      <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md group">
+        <a
+          className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+          href="#"
+        >
+          <img
+            className="object-cover w-full h-full"
+            src={Product1}
+            alt="product image"
+          />
 
-        {/* Product Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {products.map((product) => (
-            <div key={product.id} className="overflow-hidden text-left">
-              <img
-                src={product.image}
-                alt={product.title}
-                className="h-72 w-full object-cover border rounded-lg shadow-sm "
-              />
-              <div className="p-4">
-                <h3 className="font-semibold text-lg mb-2">{product.title}</h3>
-                <p className="text-gray-600">
-                  {product.price_after_discount ? (
-                    <>
-                      <span className="text-primary-default font-semibold mr-2">
-                        ${product.price_after_discount}
-                      </span>
-                      <span className="line-through text-gray-400">
-                        ${product.price}
-                      </span>
-                    </>
-                  ) : (
-                    <span>${product.price}</span>
-                  )}
-                </p>
-              </div>
+          {/* Discount */}
+          <span className="absolute top-0 left-0 m-2 rounded-full bg-red px-2 text-center text-sm font-medium text-white">
+            39% OFF
+          </span>
+
+          {/* Add to Cart Button Pop-up */}
+          <button className="absolute bottom-0 right-0 left-0 flex items-center justify-center bg-transparent text-white text-xs px-4 py-2 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <span className="flex items-center gap-1 font-medium">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              Add to Cart
+            </span>
+          </button>
+        </a>
+
+        <div className="mt-4 px-5 pb-5">
+          <a href="#">
+            <h5 className="text-xl tracking-tight text-slate-900">
+              The north coat
+            </h5>
+          </a>
+          <div className="mt-2 mb-5 flex items-center justify-between">
+            <p>
+              <span className="text-md font-bold text-black">$449</span>
+              <span className="text-md text-slate-900 line-through ml-2">
+                $699
+              </span>
+            </p>
+
+            {/* Stars */}
+            <div className="flex items-center">
+              {Array(5)
+                .fill(0)
+                .map((_, i) => (
+                  <svg
+                    key={i}
+                    aria-hidden="true"
+                    className="h-5 w-5 text-yellow-300"
+                    fill="orange"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                ))}
+
+              {/* Total Rating */}
+              <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
+                (65)
+              </span>
             </div>
-          ))}
-        </div>
-
-        {/* Pagination */}
-        <div className="flex justify-center items-center gap-4 mt-8">
-          <button
-            onClick={handlePrevPage}
-            disabled={currentPage === 1}
-            className={`px-4 py-2 text-black-500 font-medium ${
-              currentPage === 1
-                ? "text-gray-400 cursor-not-allowed"
-                : "hover:underline"
-            }`}
-          >
-            Prev
-          </button>
-
-          <button
-            onClick={handleNextPage}
-            disabled={currentPage === totalPages}
-            className={`px-4 py-2 text-black-500 font-medium ${
-              currentPage === totalPages
-                ? "text-gray-400 cursor-not-allowed"
-                : "hover:underline"
-            }`}
-          >
-            Next
-          </button>
+          </div>
         </div>
       </div>
     </section>
