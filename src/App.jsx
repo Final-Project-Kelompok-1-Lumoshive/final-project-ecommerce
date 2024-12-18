4f66c20244ffc722dc0d1229ac23962db161ed92
oreoryza <moryzasativa@gmail.com>
2024-12-19 06:21:53 +0700

    add auth branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
+import Auth from "./pages/Auth";
+
 
 const App = () => {
   return (
     <Router>
+      <div className="pb-20">
       <Navbar />
+      </div>
+      <div className="mx-32 my-32 ">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<Auth />} />
       </Routes>
+      </div>
       <Footer />
     </Router>
   );
