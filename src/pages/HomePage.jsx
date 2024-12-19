14b77023ad9e0fb40f1e5321d6a6c6ffd4fa6b98
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-12-19 11:40:55 +0700

    initial commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import ProductCard from "../components/ProductCard";
+import AllProductSection from "../components/AllProductSection";
 import TitleSection from "../components/TitleSection";
 import Banner from "../components/Banner";
 
-
 const HomePage = () => {
   return (
-
     <div className="mx-32 my-4">
-        <Banner/>
-        <TitleSection section="Categories" title="Browse By Category" />
-        <ProductCard />
+      <Banner />
+      <TitleSection section="Categories" title="Browse By Category" />
+      <AllProductSection />
     </div>
   );
 };
