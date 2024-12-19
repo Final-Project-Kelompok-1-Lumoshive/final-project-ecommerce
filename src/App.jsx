421171f7de1d289a71a43302e21eee9dbc87e25a
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-12-20 00:15:26 +0700

    feat(ProductCard): update logic product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Auth from "./pages/Auth";
 import About from "./pages/About";
@@ -10,14 +10,14 @@ const App = () => {
   return (
     <Router>
       <div className="pb-20">
-      <Navbar />
+        <Navbar />
       </div>
       <div className="mx-32 my-32 ">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
       <Footer />
     </Router>
