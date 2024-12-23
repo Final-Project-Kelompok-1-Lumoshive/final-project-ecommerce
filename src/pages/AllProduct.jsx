07026f44c8cd863f6034dad16905030a50dd242e
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-12-24 02:28:40 +0700

    feat(AllProductPage): update logic for all product section, add sidebar, add productslice function

--- src/pages/AllProduct.jsx
+++ src/pages/AllProduct.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+// src/pages/AllProduct.jsx
+
+import React, { useState } from "react";
 import AllProductSection from "../components/AllProductSection";
+import CategorySidebar from "../components/CategorySidebar";
+import { useSelector } from "react-redux";
+import { getUniqueCategories } from "../redux/async/productSlice";
 
-const Wishlist = () => {
+const AllProduct = () => {
+  const products = useSelector((state) => state.products.items);
+  const [selectedCategory, setSelectedCategory] = useState("All Products");
+
+  // Get unique categories from the products
+  const categories = ["All Products", ...getUniqueCategories(products)];
+
+  // Filter products by category
+  const filteredProducts =
+    selectedCategory === "All Products"
+      ? products
+      : products.filter((product) => product.category === selectedCategory);
+
   return (
-    <div>
+    <div className="flex">
       {/* Categories sidebar*/}
+      <CategorySidebar
+        categories={categories}
+        onSelectCategory={setSelectedCategory}
+      />
 
-      <AllProductSection />
+      {/* All products section */}
+      <div className="flex-1">
+        <AllProductSection
+          showMore={true}
+          itemsPerPage={6}
+          showPagination={false}
+          products={filteredProducts}
+        />
+      </div>
     </div>
   );
 };
 
-export default Wishlist;
+export default AllProduct;
