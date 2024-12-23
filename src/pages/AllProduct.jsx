a19eef42c006855c74a9dac2b81ea4da3a9e7c2f
Kevin Pangestu <kevin.pangestu14@gmail.com>
2024-12-23 21:21:49 +0700

    feat(allproduct): add all product page

---
+++ src/pages/AllProduct.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AllProductSection from "../components/AllProductSection";
+
+const Wishlist = () => {
+  return (
+    <div>
+      {/* Categories sidebar*/}
+
+      <AllProductSection />
+    </div>
+  );
+};
+
+export default Wishlist;
